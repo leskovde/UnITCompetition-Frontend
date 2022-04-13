8f2061b28a7fc619cd831a10995f28fdc82f9056
MojoFast <kyjonka@fastest.cz>
2022-04-13 14:07:41 +0200

    weekly

--- src/layouts/weeklyPassrate/index.js
+++ src/layouts/weeklyPassrate/index.js
@@ -1,71 +1,81 @@
-// @mui material components
 import Grid from "@mui/material/Grid";
-
-// Material Dashboard 2 React components
 import MDBox from "components/MDBox";
-
-// Material Dashboard 2 React example components
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import DashboardNavbar from "examples/Navbars/DashboardNavbar";
-
-// Dashboard components
 import VerticalBarChartWrapper from "../../additions/VerticalBarChartWrapper";
+import { useState, useEffect } from "react";
 
 async function fetchData() {
-  const from = new Date(new Date().getTime() - 1000*60*60*3).toISOString().substring(0, 19);
-  const to   = new Date(new Date().getTime() + 1000*60*60*2).toISOString().substring(0, 19);
 
-  const data = {
-    from,
-    to
-  }
+	const response = await fetch('https://unitchallenge.azurewebsites.net/api/BaseAnalysis/GetWeeklyPassRate', {
+		method: 'GET',
+		mode: 'cors',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+	});
+	return response.json();
+}
 
-  const response = await fetch('https://unitchallenge.azurewebsites.net/api/BaseAnalysis/GetWeeklyPassrate', {
-    method: 'POST',
-    mode: 'cors',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(data)
-  });
-  return response.json();
+function getWeekDays() {
+	const weekDays = [];
+	for (let i = 0; i < 7; i++) {
+		const date = new Date();
+		date.setDate(date.getDate() - i);
+		weekDays.push(date.toLocaleString('en-us', {  weekday: 'long' }));
+	}
+	return weekDays.reverse();
+};
+
+function dataTransform(inputData) {
+	const transofrmedData = []
+	for (const item of inputData) {
+		const propItem = {
+			name: ['mock name'],
+			labels: getWeekDays(),
+			datasets: [{
+				label: "Tests passed [%]",
+				color: "dark",
+				data: [15, 20, 12, 60, 20, 80, 90]
+			}]
+		}
+		propItem.name = [item.name];
+		propItem.datasets[0].data = item.weeklyPassRate.map(x => x * 100);
+		transofrmedData.push(propItem);
+	}
+
+	return transofrmedData
 }
 
 export default function WeeklyPassrate() {
-  const getWeekDays = () => {
-    const weekDays = [];
-    for (let i = 0; i < 7; i++) {
-      const date = new Date();
-      date.setDate(date.getDate() - i);
-      weekDays.push(date.toLocaleString('en-us', {  weekday: 'long' }));
-    }
+	const [data, dataSet] = useState([]);
 
-    return weekDays.reverse();
-  };
+	useEffect(() => {
+		async function fetchMyAPI() {
+			let response = await fetchData();
+			const data = dataTransform(response);
+			dataSet(data);
+		}
+	
+		fetchMyAPI()
+	}, []);
 
-  const mockData = {
-    labels: getWeekDays(),
-    datasets: [{
-      label: "Tests passed [%]",
-      color: "dark",
-      data: [15, 20, 12, 60, 20, 80, 90]
-    }]
-  };
-
-  return (
-    <DashboardLayout>
-      <DashboardNavbar />
-      <MDBox py={3}>
-        <MDBox mt={4.5}>
-          <Grid container spacing={3}>
-
-            <VerticalBarChartWrapper xs={12} md={12} lg={12}
-                                     title={"Weekly pass rate comparison"}
-                                     desc={"Pass rate in %"} data={mockData} />
-
-          </Grid>
-        </MDBox>
-      </MDBox>
-    </DashboardLayout>
-  );
+	return (
+		<DashboardLayout>
+			<DashboardNavbar />
+			<MDBox py={3}>
+				<MDBox mt={2}>
+					<Grid container spacing={3}>
+						{
+							data.map((datasets) => {
+								return <VerticalBarChartWrapper key={datasets.name} xs={12} md={12} lg={12}
+									title={datasets.name[0]}
+									desc={"Pass rate in %"} data={datasets} />
+							})
+						}
+					</Grid>
+				</MDBox>
+			</MDBox>
+		</DashboardLayout>
+	);
 }
