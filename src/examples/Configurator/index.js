6cb980351f8708d30b0998261ad7a56a25bf08a8
MojoFast <kyjonka@fastest.cz>
2022-04-13 11:51:55 +0200

    clean

--- src/examples/Configurator/index.js
+++ src/examples/Configurator/index.js
@@ -13,26 +13,17 @@ Coded by www.creative-tim.com
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 */
 
-import { useState, useEffect } from "react";
-
-// react-github-btn
-import GitHubButton from "react-github-btn";
 
 // @mui material components
 import Divider from "@mui/material/Divider";
 import Switch from "@mui/material/Switch";
 import IconButton from "@mui/material/IconButton";
-import Link from "@mui/material/Link";
 import Icon from "@mui/material/Icon";
 
-// @mui icons
-import TwitterIcon from "@mui/icons-material/Twitter";
-import FacebookIcon from "@mui/icons-material/Facebook";
 
 // Material Dashboard 2 React components
 import MDBox from "components/MDBox";
 import MDTypography from "components/MDTypography";
-import MDButton from "components/MDButton";
 
 // Custom styles for the Configurator
 import ConfiguratorRoot from "examples/Configurator/ConfiguratorRoot";
@@ -41,8 +32,6 @@ import ConfiguratorRoot from "examples/Configurator/ConfiguratorRoot";
 import {
 	useMaterialUIController,
 	setOpenConfigurator,
-	setTransparentSidenav,
-	setWhiteSidenav,
 	setFixedNavbar,
 	setSidenavColor,
 	setDarkMode,
@@ -54,78 +43,13 @@ function Configurator() {
 		openConfigurator,
 		fixedNavbar,
 		sidenavColor,
-		transparentSidenav,
-		whiteSidenav,
 		darkMode,
 	} = controller;
-	const [disabled, setDisabled] = useState(false);
 	const sidenavColors = ["primary", "dark", "info", "success", "warning", "error"];
 
-	// Use the useEffect hook to change the button state for the sidenav type based on window size.
-	useEffect(() => {
-		// A function that sets the disabled state of the buttons for the sidenav type.
-		function handleDisabled() {
-			return window.innerWidth > 1200 ? setDisabled(false) : setDisabled(true);
-		}
-
-		// The event listener that's calling the handleDisabled function when resizing the window.
-		window.addEventListener("resize", handleDisabled);
-
-		// Call the handleDisabled function to set the state with the initial value.
-		handleDisabled();
-
-		// Remove event listener on cleanup
-		return () => window.removeEventListener("resize", handleDisabled);
-	}, []);
-
 	const handleCloseConfigurator = () => setOpenConfigurator(dispatch, false);
-	const handleTransparentSidenav = () => {
-		setTransparentSidenav(dispatch, true);
-		setWhiteSidenav(dispatch, false);
-	};
-	const handleWhiteSidenav = () => {
-		setWhiteSidenav(dispatch, true);
-		setTransparentSidenav(dispatch, false);
-	};
-	const handleDarkSidenav = () => {
-		setWhiteSidenav(dispatch, false);
-		setTransparentSidenav(dispatch, false);
-	};
 	const handleFixedNavbar = () => setFixedNavbar(dispatch, !fixedNavbar);
 	const handleDarkMode = () => setDarkMode(dispatch, !darkMode);
-
-	// sidenav type buttons styles
-	const sidenavTypeButtonsStyles = ({
-		functions: { pxToRem },
-		palette: { white, dark, background },
-		borders: { borderWidth },
-	}) => ({
-		height: pxToRem(39),
-		background: darkMode ? background.sidenav : white.main,
-		color: darkMode ? white.main : dark.main,
-		border: `${borderWidth[1]} solid ${darkMode ? white.main : dark.main}`,
-
-		"&:hover, &:focus, &:focus:not(:hover)": {
-			background: darkMode ? background.sidenav : white.main,
-			color: darkMode ? white.main : dark.main,
-			border: `${borderWidth[1]} solid ${darkMode ? white.main : dark.main}`,
-		},
-	});
-
-	// sidenav type active button styles
-	const sidenavTypeActiveButtonStyles = ({
-		functions: { pxToRem, linearGradient },
-		palette: { white, gradients, background },
-	}) => ({
-		height: pxToRem(39),
-		background: darkMode ? white.main : linearGradient(gradients.dark.main, gradients.dark.state),
-		color: darkMode ? background.sidenav : white.main,
-
-		"&:hover, &:focus, &:focus:not(:hover)": {
-			background: darkMode ? white.main : linearGradient(gradients.dark.main, gradients.dark.state),
-			color: darkMode ? background.sidenav : white.main,
-		},
-	});
 
 	return (
 		<ConfiguratorRoot variant="permanent" ownerState={{ openConfigurator }}>
